ddd7d433c0b325a1263296aece24ab3fa034f33b
ikhsanmasu <76894210+ikhsanmasu@users.noreply.github.com>
2023-10-14 18:52:13 +0700

    Add files via upload
    update feature to save and reset progress

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import sys
 
-
+# use this directory when generating exe file
 if getattr(sys, 'frozen', False):
     # If the application is run as a bundle, the PyInstaller bootloader
     # extends the sys module by a flag frozen=True and sets the app
@@ -13,15 +13,16 @@ if getattr(sys, 'frozen', False):
 else:
     application_path = os.path.dirname(os.path.abspath(__file__))
 
-# csv_dir = application_path + "/indonesia_province_coordinate.csv"
-# image_dir = application_path + "/indonesia_resized.gif"
+csv_dir = application_path + "/indonesia_province.csv"
+image_dir = application_path + "/indonesia_map.gif"
+guessed_dir = application_path + "/guessed_province.csv"
 
-csv_dir = "indonesia_province_coordinate.csv"
-image_dir = "indonesia_resized.gif"
+# # use this file to simulate this file using IDE
+# csv_dir = "indonesia_province.csv"
+# image_dir = "indonesia_map.gif"
+# guessed_dir = "guessed_province.csv"
 
-curdir = os.getcwd()
 provinces = pd.read_csv(csv_dir)
-provinces_name = provinces.province.to_list()
 
 screen = Screen()
 screen.setup(width=1280, height=720)
@@ -33,12 +34,15 @@ instruction = Turtle()
 instruction.penup()
 instruction.hideturtle()
 instruction.color("black")
-instruction.goto((0, 330))
-instruction.write("ketik 'exit' untuk keluar", move=False, align="center", font=("Arial", 8, "bold"))
+instruction.goto((0, 300))
+instruction.write("ketik 'exit' untuk keluar\nketik 'reset' untuk mengulang", move=False, align="center",
+                  font=("Arial", 12, "bold"))
+
 
 def add_province_name_to_map(name):
-    x_cor = float(provinces[provinces.province == name.upper()].coordinate.to_list()[0].split(",")[0][1:])
-    y_cor = float(provinces[provinces.province == name.upper()].coordinate.to_list()[0].split(",")[1][:-1])
+    province_data = provinces[provinces.province == name]
+    x_cor = int(province_data.x.values)
+    y_cor = int(province_data.y.values)
     name_write = Turtle()
     name_write.penup()
     name_write.hideturtle()
@@ -46,11 +50,44 @@ def add_province_name_to_map(name):
     name_write.goto((x_cor, y_cor))
     name_write.write(f"{name.upper()}", move=False, align="center", font=("Arial", 8, "bold"))
 
+
+guessed_province = []
+
+try:
+    update_last_guessed = pd.read_csv(guessed_dir)
+    guessed_province = [x for x in update_last_guessed["0"]]
+except KeyError:
+    guessed_province = []
+
+for province in guessed_province:
+    add_province_name_to_map(province)
+
+all_province = provinces["province"].to_list()
+len_province = len(all_province)
+
 is_on = True
 while is_on:
-    answer_state = screen.textinput(title="Tebak semua nama provinsi", prompt="Ketik nama provinsi lainnya")
-    if answer_state.upper() in provinces_name:
-        add_province_name_to_map(answer_state)
-    elif answer_state.lower() == "exit":
-        is_on = False
+    try:
+        len_guessed = len(guessed_province)
+        answered_province = screen.textinput(title=f"{len_guessed}/{len_province} provinsi benar",
+                                             prompt="Ketik nama provinsi lainnya").upper()
+        if answered_province in all_province:
+            add_province_name_to_map(answered_province)
+            if answered_province not in guessed_province:
+                guessed_province.append(answered_province)
+        elif answered_province == "RESET":
+            guessed_province = []
+            screen.clear()
+            screen.addshape(image_dir)
+            turtle.shape(image_dir)
+            instruction.write("ketik 'exit' untuk keluar\nketik 'reset' untuk mengulang", move=False, align="center",
+                              font=("Arial", 12, "bold"))
+        elif answered_province.lower() == "exit":
+            new_data = pd.DataFrame(guessed_province)
+            new_data.to_csv(guessed_dir)
+            is_on = False
 
+    except AttributeError:
+        new_data = pd.DataFrame(guessed_province)
+        new_data.to_csv(guessed_dir)
+        is_on = False
